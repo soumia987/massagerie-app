dadf1064d23e2171d0a8badc9a2f3a6961a20983
soumia987 <soumas123456@gmail.com>
2025-06-10 15:03:18 +0100

    UserModals File

---
+++ backend/models/User.js
@@ -0,0 +1,67 @@
+const mongoose = require('mongoose');
+const bcrypt = require('bcryptjs');
+
+const userSchema = new mongoose.Schema({
+  username: {
+    type: String,
+    required: [true, 'Le nom d\'utilisateur est requis'],
+    unique: true,
+    trim: true,
+    minlength: [3, 'Le nom d\'utilisateur doit contenir au moins 3 caractères'],
+    maxlength: [30, 'Le nom d\'utilisateur ne peut pas dépasser 30 caractères']
+  },
+  email: {
+    type: String,
+    required: [true, 'L\'email est requis'],
+    unique: true,
+    trim: true,
+    match: [/^\w+([.-]?\w+)*@\w+([.-]?\w+)*(\.\w{2,3})+$/, 'Email invalide']
+  },
+  password: {
+    type: String,
+    required: [true, 'Le mot de passe est requis'],
+    minlength: [6, 'Le mot de passe doit contenir au moins 6 caractères']
+  },
+  
+  isOnline: {
+    type: Boolean,
+    default: false
+  },
+  lastSeen: {
+    type: Date,
+    default: Date.now
+  },
+  rooms: [{
+    type: mongoose.Schema.Types.ObjectId,
+    ref: 'Room'
+  }]
+}, {
+  timestamps: true
+});
+
+// Hash password before saving
+userSchema.pre('save', async function(next) {
+  if (!this.isModified('password')) return next();
+  
+  try {
+    const salt = await bcrypt.genSalt(12);
+    this.password = await bcrypt.hash(this.password, salt);
+    next();
+  } catch (error) {
+    next(error);
+  }
+});
+
+// Compare password method
+userSchema.methods.comparePassword = async function(candidatePassword) {
+  return bcrypt.compare(candidatePassword, this.password);
+};
+
+// Remove password from JSON output
+userSchema.methods.toJSON = function() {
+  const user = this.toObject();
+  delete user.password;
+  return user;
+};
+
+module.exports = mongoose.model('User', userSchema);
